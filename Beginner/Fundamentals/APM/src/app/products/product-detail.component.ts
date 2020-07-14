ffe312ce24e399a60f122f99cb16bfa3045fe355
Murilo Sanches <murilando@gmail.com>
2020-07-14 18:06:10 -0300

    Refactor modules, routing and promisses

---
+++ Beginner/Fundamentals/APM/src/app/products/product-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = "Product Detail";
+  product: IProduct;
+  errorMessage: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) { }
+
+  ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getProduct(id)
+                       .subscribe({
+                          next: (product) => this.product = product,
+                          error: err => this.errorMessage = err 
+                       });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+}
--- Beginner/Fundamentals/APM/src/app/products/product.service.ts
+++ Beginner/Fundamentals/APM/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IProduct } from './product';
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -13,10 +13,21 @@ export class ProductService {
 
     constructor(private http: HttpClient) { }
  
-    getProducts() : Observable<IProduct[]> {
+    getProducts(): Observable<IProduct[]> {
         return this.http.get<IProduct[]>(this.productUrl)
                         .pipe(
                             tap(data => console.log('All: ' + JSON.stringify(data))),
+                            catchError(this.handleError)
+                        );
+    }
+
+
+    getProduct(id: number): Observable<IProduct> {
+        return this.http.get<IProduct[]>(this.productUrl + "?productId=" + id)
+                        .pipe(
+                            
+                            tap(data => console.log("Product: " + JSON.stringify(data))),
+                            map(data => data[0] ),
                             catchError(this.handleError)
                         );
     }
